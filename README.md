59ade78ed0b3d40e8d3acca25a88779b70906c6e
Florian Mettetal <florian.mettetal@gmail.com>
2018-08-15 10:00:05 -0700

    WIP TS-React snippet updates

--- README.md
+++ README.md
@@ -6,14 +6,9 @@ WARNING: experimental, partially completed
 
 A pack of code snippets for React, Jest, GraphQL with Typescript.
 
-## Installation
+## Install
 
-In order to install an extension you need to launch the Command Pallete (Ctrl + Shift + P or Cmd + Shift + P) and type Extensions.
-There you have either the option to show the already installed snippets or install new ones.
-
-Launch VS Code Quick Open (Ctrl + P or Cmd + P), paste the following command, and press enter.
-
-`ext install tgr-snippet-pack`
+```ext install tgrstack.tgr-snippet-pack```
 
 Alternatively you can open the extensions panel and search for 'TGRStack snippet'.
 
--- build.js
+++ build.js
@@ -3,27 +3,37 @@ const OUT = './dist'
 const fs = require('fs');
 require('json5/lib/register')
 
+const callback = name => console.log(`Finished! ${name}`)
+
+// build javascript
+// - just copy the javascript.json to the output directory
+// let JS_ON = require(IN+'/js_on.json5')
+// fs.writeFile(
+//   `${OUT}/js_on.json`,
+//   JSON.stringify(JS_ON, null, 2),
+//   'utf8',
+//   callback('ts')
+// )
+
+// build typescript
+// - just copy the typescript.json to the output directory
+let TS = require(IN+'/typescript.json5')
+let TSL = require(IN+'/tslint.json5')
+fs.writeFile(
+  `${OUT}/typescript.json`,
+  JSON.stringify({...TSL, ...TS}, null, 2),
+  'utf8',
+  callback('ts')
+)
+
+// build typescript-react
 let GQL = require(IN+'/gql-apollo.json5')
 let REACT_JEST = require(IN+'/jest_react.json5')
 let JEST = require(IN+'/jest.json5')
 let REACT_FORM = require(IN+'/react_form.json5')
 let REACT_REDUX = require(IN+'/react_redux.json5')
 let REACT = require(IN+'/react.json5')
-let TSL = require(IN+'/tslint.json5')
-let TS = require(IN+'/typescript.json5')
 
-const callback = name => console.log(`Finished! ${name}`)
-
-// build typescript
-// - just copy the typescript.json to the output directory
-fs.writeFile(
-  `${OUT}/typescript.json`,
-  JSON.stringify(TS, null, 2),
-  'utf8',
-  callback('ts')
-)
-
-// build typescript-react
 const TS_REACT = {
   ...REACT_FORM,
   ...REACT_REDUX,
